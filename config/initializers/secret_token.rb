4f3c05f5147684f8ec9715ceadac3dbdfb73c96e
Laura <laura.alisson86@gmail.com>
2012-02-15 10:53:51 -0800

    Intial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LauraDow::Application.config.secret_token = 'c197aceb2ee3e196675eb8ecf80fa680cfe517c74d8b739f1b19f57e98be2b3a831ffb571dec16ef08bc3ab3098cf487e36d79736601490429384c00d03f514a'
